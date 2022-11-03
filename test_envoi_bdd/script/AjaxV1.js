6d37d986052c9e00ba73cff743207cd65337844e
danoux10 <danybarbe10@gmail.com>
2022-11-03 12:12:14 +0100

    v2

--- test_envoi_bdd/script/AjaxV1.js
+++ test_envoi_bdd/script/AjaxV1.js
@@ -1,25 +1,60 @@
 var info = document.getElementById('info');
 var tank = document.getElementById('infoTank');
+var form = document.getElementById('tankIncrement');
 
 var tankData = new XMLHttpRequest();
 
 tankData.open("GET", "function/getData.php");
-tankData.responseText = "JSON";
 tankData.send();
 
-function getData() {
-  tankData.onreadystatechange = function () {
-    console.log(this);
-    if (this.readyState == 4 && this.status == 200) {
-      var donner = JSON.parse(this.responseText);
-      console.log(donner);
-      info.innerHTML = donner;
-      tank.value = donner;
-    } else if (this.readyState == 4 && this.status == 404) {
-      alert("Error Donner First Load");
-    }
+tankData.onreadystatechange = function () {
+  console.log(this);
+  if (this.readyState == 4 && this.status == 200) {
+    var donner = JSON.parse(this.responseText);
+    console.log(donner);
+    info.innerHTML = donner;
+    tank.value = donner;
+  } else if (this.readyState == 4 && this.status == 404) {
+    alert("Error Donner First Load");
   }
 }
 
-getData();
+function getData(event){
+  event.preventDefault();
 
+  var xhrGet = new XMLHttpRequest();
+  xhrGet.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      var get = JSON.parse(this.responseText);
+      info.innerHTML = get;
+      tank.value = get;
+    }else if (this.readyState == 4 && this.status == 404){
+      alert('error get');
+    }
+  }
+  xhrGet.open('GET','function/getData.php');
+  xhrGet.send();
+  return false;
+}
+
+function sendData(event){
+  event.preventDefault();
+  var data = new FormData(this);
+  var xhrTank = new XMLHttpRequest();
+
+  xhrTank.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      console.log(this.response);
+    }else if (this.readyState == 4){
+      alert('send Data');
+    }
+  };
+
+  xhrTank.open('POST','function/sendData.php');
+  xhrTank.send(data);
+
+  return false;
+}
+
+form.addEventListener('submit', getData);
+form.addEventListener('submit', sendData);
--- test_envoi_bdd/script/sendData.js
+++ test_envoi_bdd/script/sendData.js
@@ -1,6 +1,4 @@
 var form = document.getElementById('tankIncrement');
-var information = document.getElementById('info');
-var tankInfo = document.getElementById('infoTank');
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
@@ -8,7 +6,7 @@ form.addEventListener("submit", function(e) {
   var data = new FormData(this);
 
   var xhrTank = new XMLHttpRequest();
-  // var donner = new XMLHttpRequest();
+
 
   xhrTank.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
@@ -18,38 +16,27 @@ form.addEventListener("submit", function(e) {
     }
   };
 
-  // donner.onreadystatechange = function(){
-  //   if (this.readyState == 4 && this.status == 200){
-  //     console.log(this);
-  //     information.innerHTML = JSON.parse(this.responseText);
-  //     tankInfo.value = JSON.parse(this.responseText);
-  //   }else if(this.readyState == 4 && this.status == 404){
-  //     alert('error get');
-  //   }
-  // }
-
   xhrTank.open('POST','function/sendData.php');
   xhrTank.send(data);
 
-  // donner.open('POST','function/getData.php');
-  // donner.send()//
   return false;
 });
 
-form.addEventListener('submit', function(e){
-  e.preventDefault();
+// form.addEventListener('submit', function(e){
+//   e.preventDefault();
+//
+//   var xhrGet = new XMLHttpRequest();
+//   xhrGet.onreadystatechange = function(){
+//     if (this.readyState == 4 && this.status == 200){
+//       var get = JSON.parse(this.responseText);
+//       info.innerHTML = get;
+//       tank.value = get;
+//     }else if (this.readyState == 4 && this.status == 404){
+//       alert('error get');
+//     }
+//   }
+//   xhrGet.open('GET','function/getData.php');
+//   xhrGet.send();
+//   return false;
+// });
 
-  var xhrGet = new XMLHttpRequest();
-  xhrGet.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200){
-      var get = JSON.parse(this.responseText);
-      info.innerHTML = get;
-      tank.value = get;
-    }else if (this.readyState == 4 && this.status == 404){
-      alert('error get');
-    }
-  }
-  xhrGet.open('GET','function/getData.php');
-  xhrGet.send();
-  return false;
-});
